7eb683267e06138a80b4d31e7c302f467cbe0096
daniel <caetanods1@gmail.com>
2018-09-23 15:36:53 -0400

    Organize things. Make open file functions.

--- make_Markdown/functions/make_header.R
+++ make_Markdown/functions/make_header.R
@@ -13,9 +13,3 @@ knitr::opts_chunk$set(echo = FALSE)
 ```"
 , file = handle, append = TRUE)
 }
-
-ID <- paste0(sample(1:9, size=5, replace = TRUE), collapse="")
-file.name <- file.path( getwd(), paste0("pipeline", ID, ".md") )
-handle <- file(file.name, open="a")
-
-make_header(title = "My markdown", handle = handle)
